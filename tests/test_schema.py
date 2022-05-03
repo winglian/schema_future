8a39f484ea88c8e2aae365931f49b320272f9f63
Wing Lian <wing.lian@gmail.com>
2022-05-03 16:03:15 -0400

    it's not just for nested either

--- tests/test_schema.py
+++ tests/test_schema.py
@@ -12,10 +12,17 @@ def json_schema():
         Optional("optional_map"): {
             str: Or(str, int)
         },
+        "str_or_int": Or(str, int),
     }
 
 
 def test_optional_map_type(json_schema):
-    payload = {"optional_map": {"my_bool": True}}
+    payload = {"optional_map": {"my_bool": True}, "str_or_int": 123}
+    with pytest.raises(SchemaError):
+        Schema(json_schema, ignore_extra_keys=True).validate(payload)
+
+
+def test_or_type(json_schema):
+    payload = {"str_or_int": True}
     with pytest.raises(SchemaError):
         Schema(json_schema, ignore_extra_keys=True).validate(payload)
